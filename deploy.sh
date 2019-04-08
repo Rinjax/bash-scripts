19029d2b46e357cdfbc0270c1d365735ef35e052
steve.temple <steve@encara.co.uk>
2019-04-08 15:07:45 +0100

    saving...

--- deploy.sh
+++ deploy.sh
@@ -67,7 +67,7 @@ cd $path
 git pull
 
 # Run the artisan migration process (always run)
-art migrate --force
+php $path/artisan migrate --force
 
 # Restart the queue worker if told to
 if [ $queue == 1 ]
